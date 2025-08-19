d6c8c7a7bc4f75d4e0996de308d6d6766292beaf
Jashwanth-cyber <2024eb01692@online.bits-pilani.ac.in>
2025-08-19 17:34:25 +0530

    Saving the data to db

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -132,7 +132,7 @@ function LoginContent() {
       toast.error(response.message);
       return;
     } else {
-      Cookies.set("accessToken", response.data.accessToken);
+      Cookies.set("accessToken", response.data.accessToken, { expires: 7 });
       setUserCredentials(response.data.user);
       toast.success("Login successful!");
       if (pathname.startsWith('/login') || pathname.startsWith('/signup')) {
