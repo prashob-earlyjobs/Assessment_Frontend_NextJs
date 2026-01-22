fbafaad828e1b3c9d8f7d31d6eb0a2babb626fc1
prashob-earlyjobs <prashob@earlyjobs.in>
2026-01-22 15:47:27 +0530

    og images updated

--- src/app/skill-assessments/page.tsx
+++ src/app/skill-assessments/page.tsx
@@ -4,11 +4,12 @@ import { Metadata } from "next";
 export async function generateMetadata(): Promise<Metadata> {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://www.earlyjobs.ai";
   return {
-    title: "EarlyJobs - Skill Assessments & Career Tests",
+    title: "Skill Assessments for Job Readiness | EarlyJobs",
     description:
-      "Take online skill tests, get feedback & map your career path with EarlyJobs’ precise assessments for entry level talent.",
+      "Take industry relevant skill assessments to evaluate your job readiness. Get insights into your strengths and improve your chances of getting hired with EarlyJobs.",
     keywords: [
       "skill assessments",
+      "job readiness",
       "career tests",
       "job assessments",
       "EarlyJobs",
@@ -16,26 +17,29 @@ export async function generateMetadata(): Promise<Metadata> {
       "employment opportunities"
     ],
     openGraph: {
-      title: "EarlyJobs - Skill Assessments & Career Tests",
+      title: "Skill Assessments for Job Readiness | EarlyJobs",
       description:
-        "Take online skill tests, get feedback & map your career path with EarlyJobs’ precise assessments for entry level talent.",
-      url: baseUrl,
+        "Take industry relevant skill assessments to evaluate your job readiness. Get insights into your strengths and improve your chances of getting hired with EarlyJobs.",
+      url: `${baseUrl}/skill-assessments`,
       siteName: "EarlyJobs",
       images: [
         {
-          url: `/assets/og-image.png`,
+          url: `/assets/skill_assessments.jpg`,
           width: 1200,
           height: 630,
-          alt: "EarlyJobs - Skill Assessments & Career Tests",
+          alt: "Skill Assessments for Job Readiness | EarlyJobs",
         },
       ],    
     },
     twitter: {
       card: "summary_large_image",
-      title: "EarlyJobs - Skill Assessments & Career Tests",
+      title: "Skill Assessments for Job Readiness | EarlyJobs",
       description:
-        "Take online skill tests, get feedback & map your career path with EarlyJobs’ precise assessments for entry level talent.",
-      images: [`/assets/og-image.png`],
+        "Take industry relevant skill assessments to evaluate your job readiness. Get insights into your strengths and improve your chances of getting hired with EarlyJobs.",
+      images: [`/assets/skill_assessments.jpg`],
+    },
+    alternates: {
+      canonical: `${baseUrl}/skill-assessments`,
     },
   };
 }
