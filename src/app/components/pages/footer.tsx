8ec5e998be20241757217e709a25ea726da728e2
Devesh <deveshkumarmm4@gmail.com>
2026-03-17 16:47:49 +0530

    New Sikar franchise landing page is added

--- src/app/components/pages/footer.tsx
+++ src/app/components/pages/footer.tsx
@@ -189,6 +189,13 @@ const Footer = () => {
                 Sonipat
               </p>
 
+              <p
+                onClick={() => router.push("/franchise/sikar")}
+                className="text-gray-400  text-lg hover:text-amber-600 transition-all duration-300 cursor-pointer"
+              >
+                Sikar
+              </p>
+
             </div>
           </div>
 
